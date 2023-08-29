cc116dffd8b2c7fa9de41b229eeaa9f5c7b2b5e6
Solve Otto Oren <solveottooren@gmail.com>
2023-08-29 17:49:19 +0200

    excel fungerer

--- turnus_scraper.py
+++ turnus_scraper.py
@@ -1,6 +1,8 @@
 import pdfplumber
 import pandas as pd
 import copy
+from datetime import datetime, time
+import xlsxwriter
 
 
 
@@ -13,8 +15,12 @@ linje_teller = 0
 word_remove_filter = ['Materiell:', 'Ruteterminperiode:', 'start:', 'Rutetermin:', 'Turnus:', 'Stasjoneringssted:']
 word_allow_filter = [':', 'XX', 'OO', 'TT']
 
-# Dimensjoner
 
+# Get today's date
+turnus_start_dato = datetime(2022, 12, 11)
+
+
+# Dimensjoner
 # Y-pos: top, bottom
 turnus_1_pos = [{1:(88, 115)}, {2:(115, 142)}, {3:(142, 168)}, {4:(168, 195)}, {5:(195, 222)}, {6:(222, 248)}]
 turnus_2_pos = [{1:(374, 401)}, {2:(402, 427)}, {3:(428, 454)}, {4:(455, 480)}, {5:(481, 507)}, {6:(508, 533)}]
@@ -36,55 +42,55 @@ turnuser = []
 
 turnus_mal = {1:{
                 1:{'navn':'Mandag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
-                2:{'navn':'Tirdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
+                2:{'navn':'Tirsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 3:{'navn':'Onsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 4:{'navn':'Torsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]},
-                5:{'navn': 'Fredag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
+                5:{'navn':'Fredag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 6:{'navn':'Lørdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]},
                 7:{'navn':'Søndag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}},
 
             2:{
                 1:{'navn':'Mandag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
-                2:{'navn':'Tirdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
+                2:{'navn':'Tirsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 3:{'navn':'Onsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 4:{'navn':'Torsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]},
-                5:{'navn': 'Fredag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
+                5:{'navn':'Fredag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 6:{'navn':'Lørdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]},
                 7:{'navn':'Søndag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}},
             
             3:{
                 1:{'navn':'Mandag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
-                2:{'navn':'Tirdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
+                2:{'navn':'Tirsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 3:{'navn':'Onsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 4:{'navn':'Torsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]},
-                5:{'navn': 'Fredag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
+                5:{'navn':'Fredag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 6:{'navn':'Lørdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]},
                 7:{'navn':'Søndag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}},
             
             4:{
                 1:{'navn':'Mandag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
-                2:{'navn':'Tirdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
+                2:{'navn':'Tirsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 3:{'navn':'Onsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 4:{'navn':'Torsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]},
-                5:{'navn': 'Fredag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
+                5:{'navn':'Fredag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 6:{'navn':'Lørdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]},
                 7:{'navn':'Søndag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}},
             
             5:{
                 1:{'navn':'Mandag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
-                2:{'navn':'Tirdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
+                2:{'navn':'Tirsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 3:{'navn':'Onsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 4:{'navn':'Torsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]},
-                5:{'navn': 'Fredag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
+                5:{'navn':'Fredag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 6:{'navn':'Lørdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]},
                 7:{'navn':'Søndag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}},
             
             6:{
                 1:{'navn':'Mandag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
-                2:{'navn':'Tirdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
+                2:{'navn':'Tirsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 3:{'navn':'Onsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 4:{'navn':'Torsdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]},
-                5:{'navn': 'Fredag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
+                5:{'navn':'Fredag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}, 
                 6:{'navn':'Lørdag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]},
                 7:{'navn':'Søndag', 'uke':'Uke1', 'tid':[], 'x0':[], 'dagsverk':[]}},
             }
@@ -100,10 +106,15 @@ def sorter_turnus_side(page):
     turnus2 = copy.deepcopy(turnus_mal)
 
     
+    
 
     for word in text_objects:
+        
+
         # Filtrerer ut hva som skal med videre fra pdf-en
         if (":" in word["text"] or any(sub in word["text"] for sub in word_allow_filter)) and word["text"] not in word_remove_filter:
+
+            
             # Siler ut hvilken turnus (1 eller 2)
             if int(word['top']) >= turnus_1_pos[0][1][0] and int(word['bottom']) <= turnus_1_pos[5][6][1]:
                 
@@ -114,6 +125,8 @@ def sorter_turnus_side(page):
                             for dager in dag_pos:
                                 for dag, verdi in dager.items():
                                     if word['x0'] >= verdi[0] and word['x1'] <= verdi[1]+word_crossover_tolerance:
+                                        
+
                 
                                         if dag == 8:
                                             pass
@@ -212,64 +225,110 @@ def sorter_turnus_side(page):
     turnuser.append({turnus_2_navn:turnus2})               
 
 
-def create_excel():
+def create_excel(turnus):
     
     data = {
-        'Name': ['Alice', 'Bob', 'Charlie', 'David'],
-        'Age': [25, 30, 35, 40],
-        'Department': ['HR', 'Finance', 'IT', 'Operations']
+        'Uke': [1, 2, 3, 4, 5, 6],
+        'Mandag': [],
+        'Tirsdag': [],
+        'Onsdag': [],
+        'Torsdag': [],
+        'Fredag': [],
+        'Lørdag': [],
+        'Søndag': []
     }
 
 
+
+    for turnus_navn, turnus_verdi in turnus.items():
+        
+        for uke_nr, uke in turnus_verdi.items():
+            for dag_nr, dag in uke.items():
+                
+                if len(dag['tid']) == 0:
+                    data[dag['navn']].append('')
+                else:
+                    
+   
+                    data[dag['navn']].append(" - ".join(dag['tid']))
+
+    print(data)
+
     df = pd.DataFrame(data)
 
-    print(df)
+
 
     #df.set_index('Department', inplace=True)
     
     with pd.ExcelWriter("output.xlsx", engine='xlsxwriter') as writer:
-        df.to_excel(writer, sheet_name='Sheet1', index=False)
+        df.to_excel(writer, sheet_name='Turnus 13', index=False)
         workbook  = writer.book
-        worksheet = writer.sheets['Sheet1']
+        worksheet = writer.sheets['Turnus 13']
         
      
 
 
         # Define a format for the cell background color.
-        red_format = workbook.add_format({'bg_color': '#FFC7CE',
-                                        'font_color': '#9C0006'})
-        green_format = workbook.add_format({'bg_color': '#C6EFCE',
-                                            'font_color': '#006100'})
+        tidlig_format = workbook.add_format({'bg_color': '#7abfff',
+                                        'font_color': '#000000'})
+        kveld_format = workbook.add_format({'bg_color': '#ed7777',
+                                            'font_color': '#000000'})
+        natt_format = workbook.add_format({'bg_color': '#4a4a4a',
+                                            'font_color': '#ffffff'})
+        fridag_format = workbook.add_format({'bg_color': '#13bd57',
+                                            'font_color': '#ffffff'})
+        centered_format = workbook.add_format({
+                                            'align': 'center',
+                                            'valign': 'vcenter',
+                                            'border':1})
 
         # Get the default column width for the Age column.
-        col_width = max(df['Age'].astype(str).apply(len))
+        col_width = max(df['Tirsdag'].astype(str).apply(len))
+
+
 
         # Set column width and conditional format for the Age column.
-        worksheet.set_column('B:B', col_width + 12)  # Add 2 for aesthetics.
-        worksheet.set_column('C:C', col_width + 12)  # Add 2 for aesthetics.
+        worksheet.set_column('B:H', col_width + 2)  # Add 2 for aesthetics.
+        
+        
+        # Apply centered text and borders for the range 'A1:H6'.
+        for row in range(6):
+            for col, column_label in enumerate(df.columns):
+                cell_value = df.at[row, column_label]
+                worksheet.write(row + 1, col, cell_value, centered_format) # +1 to account for header row
 
-        # Apply the conditional format to the Age column based on the value.
-        worksheet.conditional_format('B2:B5', {'type': 'cell',
-                                            'criteria': '>=',
-                                            'value': 35,
-                                            'format': red_format})
-        worksheet.conditional_format('B2:B5', {'type': 'cell',
-                                            'criteria': '<',
-                                            'value': 35,
-                                            'format': green_format})
+        for col in range(1, 8):  # Columns B(1) through H(7)
+            for row in range(1, 7):  # Rows 2 through 7
+                cell = xlsxwriter.utility.xl_rowcol_to_cell(row, col)
+                worksheet.conditional_format(cell, {'type': 'formula',
+                                                    'criteria': '=(VALUE(LEFT(' + cell + ',SEARCH(":",' + cell + ')-1))>=4)' 
+                                                    'AND (VALUE(LEFT(' + cell + ',SEARCH(":",' + cell + ')-1))<=9)'
+                                                    'AND (VALUE(MID(' + cell + ', SEARCH(":", ' + cell + ', SEARCH(":", ' + cell + ')+1)-2, 2)) < 16)',
+                                                    'format': tidlig_format})
+                
+                worksheet.conditional_format(cell, {'type': 'formula',
+                                                    'criteria': '=(VALUE(LEFT(' + cell + ',SEARCH(":",' + cell + ')-1))>=6)'
+                                                    'AND (VALUE(LEFT(' + cell + ',SEARCH(":",' + cell + ')-1))<=17)',
+                                                    'format': kveld_format})
 
-
-
-
+                worksheet.conditional_format(cell, {'type': 'formula',
+                                                    'criteria': '=(VALUE(LEFT(' + cell + ',SEARCH(":",' + cell + ')-1))>=18)'
+                                                    'AND (VALUE(LEFT(' + cell + ',SEARCH(":",' + cell + ')-1))<=23)',
+                                                    'format': natt_format})
+                worksheet.conditional_format(cell, {'type': 'formula',
+                                                    'criteria': '=(' + cell + '="")',
+                                                    'format': natt_format})
+                worksheet.conditional_format(cell, {'type': 'formula',
+                                                    'criteria': '=(' + cell + '="XX")' 'OR (' + cell + '="OO")' 'OR (' + cell + '="TT")',
+                                                    'format': fridag_format})
 
                              
 
 sorter_turnus_side(page)      
 
-#create_excel()                   
+for turnus_lst in turnuser:
+    create_excel(turnus_lst)
 
-
-print(len(turnuser))
 
 
 
