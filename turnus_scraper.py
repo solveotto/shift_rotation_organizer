0fbf0840bbb105793768cd39a6442d8804aa907c
Solve Otto <solveottooren@gmail.com>
2023-08-30 14:34:35 +0200

    plasserings alogritmen må justeres

--- turnus_scraper.py
+++ turnus_scraper.py
@@ -7,7 +7,7 @@ import xlsxwriter
 
 
 # Intitial Constants
-word_crossover_tolerance = 12
+word_crossover_tolerance = 13
 turnus_teller = 1
 
 linje_teller = 0
@@ -34,9 +34,9 @@ dag_pos = [{1:(51, 109)}, {2:(110, 167)}, {3:(167, 224)}, {4:(225, 283)},
 ### Fjernet dag åtte: , {8:(458, 514)}
 
 pdf = pdfplumber.open('turnus.pdf')
-page = pdf.pages[0]
+pages_in_pdf = pdf.pages
 
-text_objects = page.extract_words(x_tolerance = 1, y_tolerance = 1)
+#text_objects = page.extract_words(x_tolerance = 1, y_tolerance = 1)
 
 turnuser = []
 
@@ -99,14 +99,89 @@ turnus_mal = {1:{
 def sorter_turnus_side(page):
     text_objects = page.extract_words(x_tolerance = 1, y_tolerance = 1)
 
-    turnus_1_navn = text_objects[21]["text"][:-1] + " " + text_objects[23]["text"]
-    turnus_2_navn = text_objects[164]["text"][:-1] + " " + text_objects[166]["text"]
+    turnus_1_navn = 'Turnus1'
+    turnus_2_navn = 'Turnus2'
+
+
+                    # Finne og navgi turns.
+    for turnus_name in text_objects:
+
+        if turnus_name["text"] == 'RAMME' or turnus_name["text"] == 'UTLAND':
+            word_pos = text_objects.index(turnus_name)
+            if turnus_name['top'] > 0 and turnus_name['top'] < 70:
+                turnus_1_navn = text_objects[word_pos]['text']+text_objects[word_pos+1]['text']
+            elif turnus_name['top'] > 340 and turnus_name['top'] < 360:
+                turnus_2_navn = text_objects[word_pos]['text']+text_objects[word_pos+1]['text']
+
+
+        elif turnus_name["text"] == 'OSL':
+            word_pos = text_objects.index(turnus_name)
+            if turnus_name['top'] > 0 and turnus_name['top'] < 70:
+                turnus_1_navn = text_objects[word_pos]['text']+'_'+text_objects[word_pos+1]['text']
+            elif turnus_name['top'] > 340 and turnus_name['top'] < 360:
+                turnus_2_navn = text_objects[word_pos]['text']+"_"+text_objects[word_pos+1]['text']
+        
+        else:
+            pass
+
+    
+    print(turnus_1_navn, turnus_2_navn)
+
+
     turnus1 = copy.deepcopy(turnus_mal)
     turnus2 = copy.deepcopy(turnus_mal)
 
 
+    def plasserings_logikk(turnus_pos, turnus):
+        for uker in turnus_pos:  
+            for uke, uke_verdi in uker.items():
+                if word['top'] >= uke_verdi[0] and word['bottom'] <= uke_verdi[1]:
+                    
+                    for dager in dag_pos:
+                        for dag, verdi in dager.items():
+                            if word['x0'] >= verdi[0] and word['x1'] <= verdi[1]+word_crossover_tolerance:
+                                
+                                if dag == 8:
+                                    pass
+
+                                if (uke == 1 and dag == 1) or word['text'] in word_allow_filter:
+                                    turnus[uke][dag]['tid'].append(word['text'])
+                                    turnus[uke][dag]['x0'].append(word['x0'])
+                                
+                                elif uke > 1 and dag == 1:
+
+                                    if word['x0'] in turnus[uke-1][7]['x0']:
+                                        continue
+                                    if any(val in turnus[uke-1][7]['tid'] for val in word_allow_filter):
+                                        turnus[uke][dag]['tid'].append(word['text'])
+                                        turnus[uke][dag]['x0'].append(word['x0'])
+                                    elif len(turnus[uke-1][7]['tid']) == 1:
+                                        turnus[uke-1][7]['tid'].append(word['text'])
+                                        turnus[uke-1][7]['x0'].append(word['x0'])
+                                    else:
+                                        turnus[uke][dag]['tid'].append(word['text'])
+                                        turnus[uke][dag]['x0'].append(word['x0'])
+
+                                
+                                elif uke >= 1 and dag > 1:
+                                    if word['x0'] in turnus[uke][dag-1]['x0']:
+                                        continue
+                                    elif any(val in turnus[uke][dag-1]['tid'] for val in word_allow_filter):
+                                        turnus[uke][dag]['tid'].append(word['text'])
+                                        turnus[uke][dag]['x0'].append(word['x0'])
+                                    elif len(turnus[uke][dag-1]['tid']) == 1:
+                                        turnus[uke][dag-1]['tid'].append(word['text'])
+                                        turnus[uke][dag-1]['x0'].append(word['x0'])
+                                    else:
+                                        turnus[uke][dag]['tid'].append(word['text'])
+                                        turnus[uke][dag]['x0'].append(word['x0'])
+
+                                else:
+                                    turnus[uke][dag]['tid'].append(word['text'])
+                                    turnus[uke][dag]['x0'].append(word['x0'])   
+
+
     for word in text_objects:
-        
 
         # Filtrerer ut hva som skal med videre fra pdf-en
         if (":" in word["text"] or any(sub in word["text"] for sub in word_allow_filter)) and word["text"] not in word_remove_filter:
@@ -114,151 +189,54 @@ def sorter_turnus_side(page):
             
             # Siler ut hvilken turnus (1 eller 2)
             if int(word['top']) >= turnus_1_pos[0][1][0] and int(word['bottom']) <= turnus_1_pos[5][6][1]:
-                
-                for uker in turnus_1_pos:  
-                    for uke, uke_verdi in uker.items():
-                        if word['top'] >= uke_verdi[0] and word['bottom'] <= uke_verdi[1]:
-                            
-                            for dager in dag_pos:
-                                for dag, verdi in dager.items():
-                                    if word['x0'] >= verdi[0] and word['x1'] <= verdi[1]+word_crossover_tolerance:
-                                        
+                plasserings_logikk(turnus_1_pos, turnus1)
+
+
+            elif int(word['top']) >= turnus_2_pos[0][1][0]:    
+                plasserings_logikk(turnus_2_pos, turnus2)
 
                 
-                                        if dag == 8:
-                                            pass
-
-                                        if (uke == 1 and dag == 1) or word['text'] in word_allow_filter:
-                                            turnus1[uke][dag]['tid'].append(word['text'])
-                                            turnus1[uke][dag]['x0'].append(word['x0'])
-                                        
-                                        elif uke > 1 and dag == 1:
-                                            if word['x0'] in turnus1[uke-1][7]['x0']:
-                                                pass
-                                            if any(val in turnus1[uke-1][7]['tid'] for val in word_allow_filter):
-                                                turnus1[uke][dag]['tid'].append(word['text'])
-                                                turnus1[uke][dag]['x0'].append(word['x0'])
-                                            elif len(turnus1[uke-1][7]['tid']) == 1:
-                                                turnus1[uke-1][7]['tid'].append(word['text'])
-                                                turnus1[uke-1][7]['x0'].append(word['x0'])
-                                            else:
-                                                turnus1[uke][dag]['tid'].append(word['text'])
-                                                turnus1[uke][dag]['x0'].append(word['x0'])
-
-                                        
-                                        elif uke >= 1 and dag > 1:
-                                            if word['x0'] in turnus1[uke][dag-1]['x0']:
-                                                pass
-                                            elif any(val in turnus1[uke][dag-1]['tid'] for val in word_allow_filter):
-                                                turnus1[uke][dag]['tid'].append(word['text'])
-                                                turnus1[uke][dag]['x0'].append(word['x0'])
-                                            elif len(turnus1[uke][dag-1]['tid']) == 1:
-                                                turnus1[uke][dag-1]['tid'].append(word['text'])
-                                                turnus1[uke][dag-1]['x0'].append(word['x0'])
-                                            else:
-                                                turnus1[uke][dag]['tid'].append(word['text'])
-                                                turnus1[uke][dag]['x0'].append(word['x0'])
-
-                                        else:
-                                            turnus1[uke][dag]['tid'].append(word['text'])
-                                            turnus1[uke][dag]['x0'].append(word['x0'])   
 
 
-
-
-            elif int(word['top']) >= turnus_2_pos[0][1][0]:
-                
-                for uker in turnus_2_pos:  
-                    for uke, uke_verdi in uker.items():
-                        if word['top'] >= uke_verdi[0] and word['bottom'] <= uke_verdi[1]:
-                            
-                            for dager in dag_pos:
-                                for dag, verdi in dager.items():
-                                    if word['x0'] >= verdi[0] and word['x1'] <= verdi[1]+word_crossover_tolerance:
-                
-                                        if dag == 8:
-                                            pass
-                                        
-                                        if (uke == 1 and dag == 1) or word['text'] in word_allow_filter:
-                                            turnus2[uke][dag]['tid'].append(word['text'])
-                                            turnus2[uke][dag]['x0'].append(word['x0'])
-                                            
-                                        
-                                        elif uke > 1 and dag == 1:
-                                            if word['x0'] in turnus2[uke-1][7]['x0']:
-                                                pass
-                                            if any(val in turnus2[uke-1][7]['tid'] for val in word_allow_filter):
-                                                turnus2[uke][dag]['tid'].append(word['text'])
-                                                turnus2[uke][dag]['x0'].append(word['x0'])
-                                            elif len(turnus2[uke-1][7]['tid']) == 1:
-                                                turnus2[uke-1][7]['tid'].append(word['text'])
-                                                turnus2[uke-1][7]['x0'].append(word['x0'])
-                                            else:
-                                                turnus2[uke][dag]['tid'].append(word['text'])
-                                                turnus2[uke][dag]['x0'].append(word['x0'])
-
-                                        
-                                        elif uke >= 1 and dag > 1:
-                                            if word['x0'] in turnus2[uke][dag-1]['x0']:
-                                                pass
-                                            elif any(val in turnus2[uke][dag-1]['tid'] for val in word_allow_filter):
-                                                turnus2[uke][dag]['tid'].append(word['text'])
-                                                turnus2[uke][dag]['x0'].append(word['x0'])
-                                            elif len(turnus2[uke][dag-1]['tid']) == 1:
-                                                turnus2[uke][dag-1]['tid'].append(word['text'])
-                                                turnus2[uke][dag-1]['x0'].append(word['x0'])
-                                            else:
-                                                turnus2[uke][dag]['tid'].append(word['text'])
-                                                turnus2[uke][dag]['x0'].append(word['x0'])
-
-                                        else:
-                                            turnus2[uke][dag]['tid'].append(word['text'])
-                                            turnus2[uke][dag]['x0'].append(word['x0'])  
-
-
-
-    #sidens_turnuser =  [{turnus_1_navn:turnus1}, {turnus_2_navn:turnus2}]
     turnuser.append({turnus_1_navn:turnus1})
     turnuser.append({turnus_2_navn:turnus2})               
 
 
-def create_excel(turnus):
+def create_excel(data):
     
     df_dict = {}
     
     
 
 
-
-    for turnus_navn, turnus_verdi in turnus.items():
-        data = {
-        'Uke': [1, 2, 3, 4, 5, 6],
-        'Mandag': [],
-        'Tirsdag': [],
-        'Onsdag': [],
-        'Torsdag': [],
-        'Fredag': [],
-        'Lørdag': [],
-        'Søndag': []}
-        
-        for uke_nr, uke in turnus_verdi.items():
-            for dag_nr, dag in uke.items():
-                
-                if len(dag['tid']) == 0:
-                    data[dag['navn']].append('')
-                else:
+    for turnus in data:
+        for turnus_navn, turnus_verdi in turnus.items():
+            df_data = {
+            'Uke': [1, 2, 3, 4, 5, 6],
+            'Mandag': [],
+            'Tirsdag': [],
+            'Onsdag': [],
+            'Torsdag': [],
+            'Fredag': [],
+            'Lørdag': [],
+            'Søndag': []}
+            
+            for uke_nr, uke in turnus_verdi.items():
+                for dag_nr, dag in uke.items():
                     
-   
-                    data[dag['navn']].append(" - ".join(dag['tid']))
-
-        df_dict[turnus_navn] = pd.DataFrame(data)
-
-
+                    if len(dag['tid']) == 0:
+                        df_data[dag['navn']].append('')
+                    else:
+                        
+    
+                        df_data[dag['navn']].append(" - ".join(dag['tid']))
+            
+            df_dict.update({turnus_navn : pd.DataFrame(df_data)})
 
     
     with pd.ExcelWriter("output.xlsx", engine='xlsxwriter') as writer:
         for sheet_name, df in df_dict.items():
-            print(df)
+            
 
             df.to_excel(writer, sheet_name=sheet_name, index=False)
             workbook  = writer.book
@@ -283,13 +261,12 @@ def create_excel(turnus):
                                                 'valign': 'vcenter',
                                                 'border':1})
 
-            # Get the default column width for the Age column.
-            col_width = max(df['Tirsdag'].astype(str).apply(len))
-
-
+            
+        
 
             # Set column width and conditional format for the Age column.
-            worksheet.set_column('B:H', col_width + 2)  # Add 2 for aesthetics.
+            worksheet.set_column('B:H', 12) 
+            worksheet.set_column('A:A', 4)
             
             
             # Apply centered text and borders for the range 'A1:H6'.
@@ -329,12 +306,14 @@ def create_excel(turnus):
                                                         'criteria': '=(' + cell + '="XX")' 'OR (' + cell + '="OO")' 'OR (' + cell + '="TT")',
                                                         'format': fridag_format})
 
+
+
                              
+for page in pages_in_pdf[7:10]:
+    sorter_turnus_side(page)      
 
-sorter_turnus_side(page)      
+create_excel(turnuser)
 
-for turnus_lst in turnuser:
-    create_excel(turnus_lst)
 
 
 
@@ -348,3 +327,55 @@ for turnus_lst in turnuser:
 #                 for g, h in uke_value.items():
 #                     print(turnus, uke_nr, g, h['tid'], "-", h['x0'])
 #                     print(" ")
+
+
+
+
+
+#### BACKUP AV TURNUS2 SORTERING:
+# for uker in turnus_2_pos:  
+                #     for uke, uke_verdi in uker.items():
+                #         if word['top'] >= uke_verdi[0] and word['bottom'] <= uke_verdi[1]:
+                            
+                #             for dager in dag_pos:
+                #                 for dag, verdi in dager.items():
+                #                     if word['x0'] >= verdi[0] and word['x1'] <= verdi[1]+word_crossover_tolerance:
+                
+                #                         if dag == 8:
+                #                             pass
+                                        
+                #                         if (uke == 1 and dag == 1) or word['text'] in word_allow_filter:
+                #                             turnus2[uke][dag]['tid'].append(word['text'])
+                #                             turnus2[uke][dag]['x0'].append(word['x0'])
+                                            
+                                        
+                #                         elif uke > 1 and dag == 1:
+                #                             if word['x0'] in turnus2[uke-1][7]['x0']:
+                #                                 pass
+                #                             if any(val in turnus2[uke-1][7]['tid'] for val in word_allow_filter):
+                #                                 turnus2[uke][dag]['tid'].append(word['text'])
+                #                                 turnus2[uke][dag]['x0'].append(word['x0'])
+                #                             elif len(turnus2[uke-1][7]['tid']) == 1:
+                #                                 turnus2[uke-1][7]['tid'].append(word['text'])
+                #                                 turnus2[uke-1][7]['x0'].append(word['x0'])
+                #                             else:
+                #                                 turnus2[uke][dag]['tid'].append(word['text'])
+                #                                 turnus2[uke][dag]['x0'].append(word['x0'])
+
+                                        
+                #                         elif uke >= 1 and dag > 1:
+                #                             if word['x0'] in turnus2[uke][dag-1]['x0']:
+                #                                 pass
+                #                             elif any(val in turnus2[uke][dag-1]['tid'] for val in word_allow_filter):
+                #                                 turnus2[uke][dag]['tid'].append(word['text'])
+                #                                 turnus2[uke][dag]['x0'].append(word['x0'])
+                #                             elif len(turnus2[uke][dag-1]['tid']) == 1:
+                #                                 turnus2[uke][dag-1]['tid'].append(word['text'])
+                #                                 turnus2[uke][dag-1]['x0'].append(word['x0'])
+                #                             else:
+                #                                 turnus2[uke][dag]['tid'].append(word['text'])
+                #                                 turnus2[uke][dag]['x0'].append(word['x0'])
+
+                #                         else:
+                #                             turnus2[uke][dag]['tid'].append(word['text'])
+                #                             turnus2[uke][dag]['x0'].append(word['x0'])  
