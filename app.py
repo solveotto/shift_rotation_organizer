2fe8b5a248bf47e39f8427e5276853cca180d3fa
Sølve Otto Øren <solveottooren@gmail.com>
2024-07-31 21:59:59 +0200

    menysystem OK

--- app.py
+++ app.py
@@ -11,12 +11,15 @@ class DataframeManager():
     def __init__(self) -> None:
         self.df = pd.read_json('turnus_df_R24.json')
         self.helgetimer_mulip = 1
+        self.ettermiddager = 0
+        self.ettermiddag_poeng = 0
 
         self.update_df()
 
     def update_df(self):
         self.calc_helgetimer()
-        self.calc_netter()
+        self.calc_ettermiddag_vakter()
+        #self.calc_netter()
         self.df = self.df.sort_values(by='poeng')
 
 
@@ -25,6 +28,16 @@ class DataframeManager():
 
     def calc_netter(self):
         self.df.loc[self.df['natt'] > 6, 'poeng'] += 1
+
+    def calc_ettermiddag_vakter(self):
+        threshold = self.ettermiddager  # Specific threshold
+        multiplier = self.ettermiddag_poeng  # Amount to multiply
+
+        # Iterate through each row in the DataFrame
+        for index, row in self.df.iterrows():
+            if row['ettermiddag'] > threshold:
+                # Multiply the 'ettermiddag' value by the specified amount
+                self.df.at[index, 'poeng'] += (row['ettermiddag'] - threshold) * multiplier
 
 
         
@@ -38,11 +51,19 @@ def home():
     table_data = df_manager.df.to_dict(orient='records')
 
     helgetimer = session.get('helgetimer', '0')
+    ettermiddager = session.get('ettermiddager', '0')
+    ettermiddager_poeng = session.get('ettermiddager_poeng', '0')
 
+   
     session.clear()
 
     # Pass the table data to the template
-    return render_template('index.html', table_data=table_data, helgetimer=helgetimer)
+    return render_template('index.html', 
+                           table_data = table_data, 
+                           helgetimer = helgetimer,
+                           ettermiddager = ettermiddager,
+                           ettermiddager_poeng = ettermiddager_poeng
+                           )
 
 
 @app.route('/submit', methods=['POST'])
@@ -52,13 +73,20 @@ def calulate():
    
     helgetimer = request.form.get('helgetimer', '0')
     df_manager.helgetimer_mulip = int(helgetimer)
+    session['helgetimer'] = helgetimer
 
-
+    ettermiddager = request.form.get('ettermiddager', '0')
+    df_manager.ettermiddager = int(ettermiddager)
+    session['ettermiddager'] = ettermiddager
+    
+    ettermiddager_poeng = request.form.get('ettermiddager_poeng', '0')
+    df_manager.ettermiddag_poeng = int(ettermiddager_poeng)
+    session['ettermiddager_poeng'] = ettermiddager_poeng
+    
     
     
     df_manager.update_df()
     
-    session['helgetimer'] = helgetimer
     
     
     
