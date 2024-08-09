aaeed994e2329708016bdb918d99c6faf2643792
Solve Otto <solveottooren@gmail.com>
2024-08-09 22:03:47 +0200

    sql fun

--- sql_test.py
+++ sql_test.py
@@ -1,13 +1,54 @@
 from getpass import getpass
 from mysql.connector import connect, Error
 
+
+
+create_movies_table_query = """
+CREATE TABLE movies(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100),
+    release_year YEAR(4),
+    genre VARCHAR(100),
+    collection_in_mil INT
+)
+"""
+
+create_reviewers_table_query = """
+CREATE TABLE reviewers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100)
+)
+"""
+
+create_ratings_table_query = """
+CREATE TABLE ratings (
+    movie_id INT,
+    reviewer_id INT,
+    rating DECIMAL(2,1),
+    FOREIGN KEY(movie_id) REFERENCES movies(id),
+    FOREIGN KEY(reviewer_id) REFERENCES reviewers(id),
+    PRIMARY KEY(movie_id, reviewer_id)
+)
+"""
+
+show_table_query = "DESCRIBE movies"
+
 try:
     with connect(
         host="localhost",
         user=input("Enter username: "),
         password=getpass("Enter password: "),
+        database="online_movie_rating",
     ) as connection:
-        print(connection)
+        with connection.cursor() as cursor:
+            cursor.execute(show_table_query)
+            result = cursor.fetchall()
+            for row in result:
+                print(row)
 except Error as e:
     print(e)
-    
+
+
+
+
