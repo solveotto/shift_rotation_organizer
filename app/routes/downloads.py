4049cc2f07816f98adcd1d0f3dca566d00b90df9
Sølve Otto Øren <solveottooren@gmail.com>
2025-09-28 07:44:18 +0200

    Ui er klar for produksjon

--- app/routes/downloads.py
+++ app/routes/downloads.py
@@ -6,6 +6,7 @@ downloads = Blueprint('downloads', __name__)
 @downloads.route('/download_excel')
 def download_excel():
     filename = f'{conf.CURRENT_TURNUS}/turnuser_R25.xlsx'  # Replace with your actual file name
+    print(f'{conf.CURRENT_TURNUS}/turnuser_R25.xlsx')
     return send_from_directory(conf.static_dir, filename, as_attachment=True)
 
 @downloads.route('/download_turnusnokler_zip')
